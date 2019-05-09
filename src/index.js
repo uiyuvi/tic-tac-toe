e9089a8ae9774ae2dc65518cd42c57353e01929a
Anonymous <>
2019-05-09 20:08:27 +0200

    refactor: Extract position from board to seperate function

--- src/index.js
+++ src/index.js
@@ -5,15 +5,19 @@ var gameView = new GameView();
 var game = new GamePresenter(gameView);
 
 var makeMove = function (event) {
-    var row = lastCharacter(event.target.parentElement.id);
-    var column = lastCharacter(event.target.className);
+    var row = getRow(event);
+    var column = getColumn(event);
     game.move(row, column);
 }
-
+var getRow = function (event) {
+    return lastCharacter(event.target.parentElement.id);
+}
+var getColumn = function (event) {
+    return lastCharacter(event.target.className);
+}
 var lastCharacter = function(string){
     return string.slice(-1);
 }
-
 const positions = document.querySelectorAll('.row span');
 for(var position = 0;position <= positions.length; position++){
     positions[position].addEventListener('click', function (event) {
